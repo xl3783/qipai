4366bbaf9c1a04ec6c4291685368bc7150da2163
xiaolei <xiaolei_sz2024@163.com>
2025-07-24 10:49:35 +0800

    page index with v0

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,14 +1,16 @@
+
 export default defineAppConfig({
   pages: [
-    'pages/index/index',
-    'pages/room/index',
-    'pages/settlement/index',
-    'pages/game-history/index'
+    'pages/profile/index',
+    // 'pages/index/index',
+    // 'pages/battle-records/index',
+    // 'pages/settlement/index',
+    // 'pages/transfer/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: '室友记账',
     navigationBarTextStyle: 'black'
-  }
+  },
 })
